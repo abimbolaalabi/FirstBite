51e3bed739e4d65f31e01ac0aa9411a560398025
Luxurybarbie <jonahokam@gmail.com>
2025-09-28 01:41:59 +0100

    Login done

--- src/Components/Landing/Login/Login.jsx
+++ src/Components/Landing/Login/Login.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { IoCloseOutline } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+     <div className='modal'>
+            <form className='login_container'>
+             
+             <div className='log'>
+                <div style={{display:"flex", justifyContent:"flex-end"}}><IoCloseOutline size={25}/></div>
+                <h3 className='login'>Login</h3>
+                <h4 className='login_to'>Login to continue</h4>
+             </div> 
+                <div className='body'>
+                    <label>Email</label>
+                    <input type='text' placeholder='Enter your email' required/>  
+                </div>
+                 <div className='psw'>
+                    <label>Pasword</label>
+                    <input type='password' placeholder='Enter your password' required/>
+                 </div>
+                
+                 <p className='fpsw'>Forgotten password? <span style={{color:"#ff7700"}}>Click here</span></p>
+
+                <div className='log_al'>
+                    <button className='log_atl_log'>Login</button>
+                    <div className='divider'>
+                       <span>or</span>
+                    </div>
+                    <button className='btn' style={{display:"flex", alignItems:"center", justifyContent:"center", gap:"0.5rem"}}><FcGoogle size={25}/>Sign in with Google</button>
+                </div>
+            
+                <p className='acct'>Don't have an account? <span style={{color:"#ff7700"}}>Create an account</span></p>
+      
+    </form>
+     </div>
   )
 }
 
